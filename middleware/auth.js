11cc4f8966f3c6c06cd29ef737fd058e83baed73
M. Jony Sheikh <108057648+smjoni35@users.noreply.github.com>
2026-09-14 05:20:36 +0600

    Add files via upload

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,4 +5,18 @@ function requireLogin(req, res, next) {
   next();
 }
 
-module.exports = { requireLogin };
+function requireAdmin(req, res, next) {
+  if (!req.session.user) {
+    return res.redirect('/login');
+  }
+  if (req.session.user.role !== 'admin') {
+    return res.status(403).render('error', {
+      user: req.session.user,
+      activeNav: '',
+      message: 'এই কাজটি শুধু অ্যাডমিন করতে পারবেন।'
+    });
+  }
+  next();
+}
+
+module.exports = { requireLogin, requireAdmin };
